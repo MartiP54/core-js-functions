8ddc3033ca2952a659e84db37ac5205ed2aa2af4
MartiP54 <116718918+MartiP54@users.noreply.github.com>
2024-04-14 21:29:04 +0700

    add 8 task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -195,8 +195,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function sut(...arg) {
+    return fn.call(this, ...args1, ...arg);
+  };
 }
 
 /**
@@ -216,8 +218,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let count = startFrom;
+  return function plus() {
+    const result = count;
+    count += 1;
+    return result;
+  };
 }
 
 module.exports = {
